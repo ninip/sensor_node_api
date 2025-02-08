be330ddb445be94c580830d80cd08c2430837248
Naomi <16569445+ninip@users.noreply.github.com>
2025-02-07 18:14:09 -0800

    Update sensor_service.py

--- src/services/sensor_service.py
+++ src/services/sensor_service.py
@@ -8,8 +8,8 @@ class SensorService(BaseService):
         super().__init__(Sensor)
 
     def connect_to_node(self, sensor_id: int, node_id: int):
-        sensor = self.get_by_id(sensor_id)
-        if sensor:
-            sensor.node_id = node_id
-            db.session.commit()
-        return sensor
+        with session_scope() as session:
+            sensor = session.query(self.model).get(sensor_id)
+            if sensor:
+                sensor.node_id = node_id
+            return sensor
